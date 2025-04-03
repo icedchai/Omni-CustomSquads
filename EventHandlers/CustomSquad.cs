de88dfc4057c0df20ef268437bde16fd4ace8f45
icedchai <70030002+icedchai@users.noreply.github.com>
2025-04-03 17:21:37 -0500

    Update CustomSquad.cs

--- EventHandlers/CustomSquad.cs
+++ EventHandlers/CustomSquad.cs
@@ -19,19 +19,19 @@
         /// Gets or sets a value indicating whether to make a CASSIE announcement upon this unit's arrival.
         /// </summary>
         [Description("Whether to make a CASSIE announcement")]
-        public bool UseCassieAnnouncement { get; set; }
+        public bool UseCassieAnnouncement { get; set; } = true;
 
         /// <summary>
         /// Gets or sets the squad name.
         /// </summary>
         [Description("Name used to refer to the squad in commands and logs")]
-        public string SquadName { get; set; }
+        public string SquadName { get; set; } = "delta4";
 
         /// <summary>
         /// Gets or sets the spawn wave this replaces.
         /// </summary>
         [Description("Respawn wave this will replace. Use NtfWave to get the NATO divisions (Juliett-15), and ChaosWave to not.")]
-        public SpawnableFaction SquadType { get; set; }
+        public SpawnableFaction SquadType { get; set; } = SpawnableFaction.NtfWave;
 
         [Description("Chance this squad spawns instead of vanilla, or the other squads. Added up with vanilla spawn chance and other squad spawn chances.")]
         public int SpawnChance { get; set; } = 0;
@@ -40,12 +40,12 @@
         /// Gets or sets the CASSIE announcement this squad will use.
         /// </summary>
         [Description("Announcement CASSIE will say when the custom squad enters")]
-        public string EntranceAnnouncement { get; set; }
+        public string EntranceAnnouncement { get; set; } = "mtfunit delta 4 designated minute men division %division% hasentered";
 
         /// <summary>
         /// Gets or sets the subtitles this CASSIE announcement will use.
         /// </summary>
-        public string EntranceAnnouncementSubs { get; set; }
+        public string EntranceAnnouncementSubs { get; set; } = "Mobile Task Force Unit Delta-4 designated 'Minutemen', division %division%, has entered the facility.";
 
         /// <summary>
         /// Gets or sets a value indicating whether or not to use the default team vehicle (eg. CI Van, NTF Helo).
@@ -63,7 +63,8 @@
         [Description("Role type corresponding to the letters in the spawn queue. ONLY PUT one character!!")]
         public Dictionary<char, OverallRoleType> CustomRoles { get; set; } = new Dictionary<char, OverallRoleType>
         {
-            { '0', new OverallRoleType { RoleId = 6, RoleTypeSystem = TypeSystem.BaseGame } },
+            { '0', new OverallRoleType { RoleId = 6, RoleType = TypeSystem.BaseGame } },
+            { '1', new OverallRoleType { RoleId = 7, RoleType = TypeSystem.BaseGame } },
         };
 
         [Description("Put a string of numbers or letters corresponding to the custom-role lookup system above.")]
